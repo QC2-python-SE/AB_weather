555d070cc65b06314875d7185ea36419407d4661
Stasiu Wolanski <stasiu.wolanski@riverlane.com>
2024-10-14 15:41:48 +0100

    added variance calculation file

---
+++ calculate_variance.py
@@ -0,0 +1,13 @@
+
+
+from numbers import Number
+from typing import List
+
+
+def calculate_variance(data: List[Number]) -> Number:
+    if not isinstance(data, List):
+        raise TypeError("Should be a list of numbers")
+    if not len(data) > 0:
+        raise ValueError("Should be more than zero points.")
+    mean = sum(data)/len(data)
+    return sum((x - mean)**2 for x in data)/len(data)

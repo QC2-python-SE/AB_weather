0e6bbb24d0520713421901c2914ba6b7d021cd2e
Joel Mills <be20071@bristol.ac.uk>
2024-10-23 14:49:09 +0100

    Send stuff to eleanor

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,12 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../../abweather'))
+
+
 project = 'AB_Weather'
 copyright = '2024, qwacobec'
 author = 'qwacobec'
@@ -14,15 +20,14 @@ release = '0.01'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinc.ext.autodoc', 'sphinx.ext.napoleon']
 
 templates_path = ['_templates']
 exclude_patterns = []
 
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
